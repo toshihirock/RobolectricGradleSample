800fc953d32e7d60267a31073755d4af6cb3cb61
toshihirock <toshihiro308@gmail.com>
2014-10-01 23:43:20 +0900

    first commit

---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public int sum(int a, int b) {
+    	return a + b;
+    }
+}
---
+++ test/java/com/example/hellowrold/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.example.hellowrold;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import com.example.helloworld.MainActivity;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+	
+	public MainActivity activity;
+	
+	@Before
+	public void setup() {
+		this.activity = Robolectric.buildActivity(MainActivity.class).create().get();
+	}
+	
+	@Test
+	public void shouldHaveHelloWorld() throws Exception {
+		int expected = 4;
+		int actual = activity.sum(1, 3);
+		assertThat(actual, is(expected));
+	}
+
+}
